f9f0c741562e303ee69e1349ff68839ee4deb659
Bryn Barton <brynsdailyjournal@gmail.com>
2019-03-16 11:42:39 -0500

    fin

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 
 //The quotes and their properties that will show up on the webpage
-var backGroundColor = [ 'red', 'blue', 'yellow', 'purple', 'black']
+var backGroundColor = [ 'red', 'blue', 'yellow', 'purple', 'black'];
 var quotes = [
   {
     quote: "I don't care about wars, I wish all people in world become friends",
@@ -36,27 +36,27 @@ function getRandomQuote() {
   var randomNumber = Math.floor(Math.random() * quotes.length);
   return quotes[randomNumber];
 };
-
+//this function will assign the random color for printQuote to display
 function getRandomColor() {
   var randomColor = Math.floor(Math.random()* backGroundColor.length);
   return backGroundColor[randomColor];
-}
+};
 
 
 
 
-//This function will run when the button is clicked, causing the new quotes and properties to show on the page, depending on the number chosen by getRandomQuote().
+//This function will run when the button is clicked, causing the new quotes and properties to show on the page, depending on the number chosen by getRandomQuote() and getRandomColor().
 function printQuote() {
   var results = getRandomQuote();
   var theHTML = '<p class="quote">' + results.quote + '</p>' +
                 '<p class="source">' + results.source;
                 if (results.citation !== undefined) {
-                theHTML += '<span class="citation">' + results.citation + '</span>'
+                theHTML += '<span class="citation">' + results.citation + '</span>';
                 } if (results.year !== undefined) {
-                theHTML += '<span class="year">' + results.year + '</span>'
+                theHTML += '<span class="year">' + results.year + '</span>';
               } if (results.category !== undefined) {
-                theHTML += '<span class="category">' + results.category + '</span>'
-              };
+                theHTML += '<span class="category">' + results.category + '</span>';
+              }
                 theHTML += '</p>'
 document.getElementById('quote-box').innerHTML = theHTML;
 document.body.style.backgroundColor = getRandomColor();
